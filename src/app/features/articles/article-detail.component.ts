d3368ddf2b95c38d06a5c87ea7f028c65b347e8d
Rinot RAKOTOMALALA <130461482+Fiainana@users.noreply.github.com>
2026-09-25 09:46:09 +0200

    Fiche article simplifiée : pas de prix d'achat, stock disponible seulement

--- src/app/features/articles/article-detail.component.ts
+++ src/app/features/articles/article-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, inject, signal } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { DecimalPipe } from '@angular/common';
 import { ArticlesService } from '../../core/services/articles.service';
-import { Article, ArticleStockDepot } from '../../core/models/article.model';
+import { Article } from '../../core/models/article.model';
 
 @Component({
   selector: 'app-article-detail',
@@ -19,10 +19,6 @@ export class ArticleDetailComponent implements OnInit {
   readonly loading = signal(true);
   readonly error = signal<string | null>(null);
   readonly article = signal<Article | null>(null);
-  readonly stocks = signal<ArticleStockDepot[]>([]);
-  readonly stockTotal = signal(0);
-  readonly stockReserve = signal(0);
-  readonly stockCommande = signal(0);
   readonly stockDisponible = signal(0);
 
   ngOnInit(): void {
@@ -40,11 +36,9 @@ export class ArticleDetailComponent implements OnInit {
     this.articlesApi.getByReference(reference).subscribe({
       next: (data) => {
         this.article.set(data.article);
-        this.stocks.set(data.stocksParDepot ?? []);
-        this.stockTotal.set(data.stockTotal ?? 0);
-        this.stockReserve.set(data.stockReserve ?? 0);
-        this.stockCommande.set(data.stockCommande ?? 0);
-        this.stockDisponible.set(data.stockDisponible ?? 0);
+        this.stockDisponible.set(
+          data.stockDisponible ?? data.article.stockDisponible ?? 0
+        );
         this.loading.set(false);
       },
       error: (err) => {
